d534425dd8ed5cadfd0f6bf57306a3b0262d9c73
Maurício Antunes <mauricio.abreua@gmail.com>
2024-07-02 21:10:56 -0300

    RTMP + encoding + LLHLS

--- llhls.sh
+++ llhls.sh
@@ -2,6 +2,8 @@
 
 rm -r llhls/*
 
-gpac flist:srcs=big_buck_bunny.mp4:floop=-1 reframer:rt=sync -o llhls/live.m3u8:segdur=2:cdur=0.5:profile=live:dmode=dynamic:profile=live:muxtype=ts:llhls=sf
+#gpac flist:srcs=big_buck_bunny.mp4:floop=-1 reframer:rt=sync -o llhls/live.m3u8:segdur=2:cdur=0.5:profile=live:dmode=dynamic:profile=live:muxtype=ts:llhls=sf
 
-#gpac -i rtmp://localhost:1935/llhls -o llhls/live.m3u8:segdur=2:cdur=0.5:profile=live:dmode=dynamic:profile=live:muxtype=ts
+gpac -i rtmp://localhost:1935/llhls @@ c=avc:b=1300k:FID=S1M @@ ffsws:osize=630x360 @ \
+    c=avc:b=2000k:FID=S2M @@ ffsws:osize=768x432 \
+    -o http://localhost:8081/llhls/live.m3u8:gpac:segdur=2:cdur=0.5:profile=live:dmode=dynamic:rdirs=llhls:profile=live:muxtype=ts:llhls=sf:SID=S1M,S2M
